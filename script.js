5b97ab516942ba0bd3903e4866b37efead9615a8
Atara Levi <98110096+atara100@users.noreply.github.com>
2022-05-15 00:45:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let btnCounter=0;
+let productsArr=[];
+let evt;
+
+function display(){
+fetch("http://localhost:3003/myproductslist")
+.then(response=>response.json())
+.then(responseP=>displayList(responseP.productsList));
+};
+
+
+function displayList(productsMames){
+    productsArr=productsMames;
+    productsMames.forEach(element => {
+        document.getElementById('card').innerHTML+=
+        `<div class="cardP">
+         <h1>${element.name}</h1>
+         <img  src="./images/${element.name}.jpg">
+         <h4>Price: ${element.price}</h4>
+         <button class="btnSale" id="${element.name}"> check sale </button>
+        </div>`;
+    });
+}
+
+document.addEventListener("click",productSale);
+
+function productSale(event){
+ evt=event.target;
+ if(evt.classList[0]=="btnSale"){
+       fetch("http://localhost:3003/sales")
+        .then(response => response.json())
+        .then(responseSale=>displatDiscount(responseSale));
+ }
+}
+
+function displatDiscount(discount){
+    if(evt.id=="Necklace"){
+        alert(`we have discount of ${discount.Necklace} ❤!!!`);
+    }
+    else if(evt.id=="Ring"){
+        alert(`we have discount of ${discount.Ring} ❤!!!`);
+    }else{
+        alert("sorry, we have not sale on this product 😞");
+    }
+}
+
+
+
